6eb9690d0d468858d7f2122abb9aba257c41c093
Justin Spears <jspears@netflix.com>
2017-07-19 00:44:04 -0700

    fixed export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-export ExpandableScroller from './ExpandableScroller';
+import _ExpandableScroller, { ToggleItem as _ToggleItem } from './ExpandableScroller';
+
 export default from './Scroller'
 
+export const ToggleItem         = _ToggleItem;
+export const ExpandableScroller = _ExpandableScroller;
