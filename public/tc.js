42f048888d7e787ec639df1bf27e773a30fc27f0
Justin Spears <jspears@netflix.com>
2017-07-21 15:54:08 -0700

    clean up style

--- public/tc.js
+++ public/tc.js
@@ -1,7 +1,8 @@
 import theme, { themeClass } from '../src/themes/index';
 import AppStyle from './App.stylm';
+import App from './App';
 
 theme({ App: AppStyle });
 
-const tc = themeClass({ displayName: 'App' });
+const tc = themeClass(App);
 export default tc;
